1bc49a8416ff956b0fbee19c7546a6af42893730
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-03-22 13:46:00 +0530

    Add new file - practice file for interview

--- hello.py
+++ hello.py
@@ -1,1 +1,54 @@
-print("hello,kanha")
+# # Example 1
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Neha")
+
+# # Example 2
+# def add(a, b):
+#     return a + b
+
+# result = add(5, 3)
+# print(result)
+
+# def big(a ,b):
+#     if(a>b):
+#         print(a)
+#     else:
+#         print(b)
+# big(10,9)
+
+# a = int(input("enter number:"))
+# def even(a):
+#     if(a%2 == 0):
+#         print("number is even :",a)
+#     else:
+#         print("number is odd : ",a)
+# even(a)
+
+#(p*r*t)/100
+
+# P = int(input("Enter Original amount: "))
+# R = int(input("Enter Rate :"))
+# T = int(input("Enter time :"))
+
+# def SI(P,R,T):
+#     print("Simple interst is ")
+#     print((P*R*T)/100)
+
+# SI(P,R,T)
+
+#loop
+# l = ["Mango","Apple","Banana","Kiwi","Watemelon"]
+# for item in l:
+#     print(l)
+
+# in this code we learn that item is placeholder that contain one item each at a time and prints list
+
+# l = ["Mango","Apple","Banana","Kiwi","Watemelon"]
+# l.insert(1,"cherry")
+# l.pop(3)
+# print(l)
+
+#prime 1oritself
+
