a2932f335f38cb190c42b5f59acab164faa9b407
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-13 17:08:23 +0530

    recent update

--- chapter10_OOP/03_self_parameter.py
+++ chapter10_OOP/03_self_parameter.py
@@ -2,10 +2,14 @@ class Employee:
     salary = 120000
     language = "Python"
 
-def getinfo(self):
-    print(f"The language is {self.language} . The salary is {self.salary} ")
+    def getinfo(self):
+        print(f"The language is {self.language} . The salary is {self.salary} ")
+    
+    def greet(self):
+        print("Good morning")
     
 
 harry = Employee()
 harry.language = "Javascript"
+harry.getinfo()
 harry.getinfo()
