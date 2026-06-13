5fedc564868430f25a32a09ae3b5d0f48f0f5fdf
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-13 15:40:33 +0530

    recent update

---
+++ chapter10_OOP/01_class.py
@@ -0,0 +1,12 @@
+# syntax
+class Empolyee:
+    name ="neha"
+    language = "py"
+    salary = 120000
+    
+    
+neha = Empolyee()
+print(neha.name , neha.language)
+
+
+Here name is object attribute and 
