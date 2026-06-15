a75bb5f28d0e852e5d4708e6bfc8a53322488d7e
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-15 21:51:33 +0530

    recent update

--- chapter10_OOP/practice_question/2.problem.py
+++ chapter10_OOP/practice_question/2.problem.py
@@ -1,1 +1,18 @@
 # 2. Write a class “Calculator” capable of finding square, cube and square root of a number
+class calc:
+    def __init__(self,n):
+        self.n = n;
+    
+    def square(self):
+        print(f"The Square is {self.n*self.n}")
+    
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+    
+    def sqrt(self):
+        print(f"The Square root is {self.n**1/2}")
+    
+a = calc(4)
+a.square()
+a.cube()
+a.sqrt()
