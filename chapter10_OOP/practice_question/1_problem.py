4edd3b136ba81a58dfc1293e3a3bad1a41397d39
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-15 21:23:33 +0530

    recent update

---
+++ chapter10_OOP/practice_question/1_problem.py
@@ -0,0 +1,16 @@
+# 1. Create a class “Programmer” for storing information of few programmers 
+# working at Microsoft. 
+
+class programmer:
+    comapny = "Microsoft"
+    
+    def __init__(self , name , salary , role, pincode):
+        self.name = name
+        self.salary = salary 
+        self.role = role
+        self.pincode =pincode
+        
+        print("Information of Employees working at Microsoft")
+        
+harry = programmer("Harry","Python Developer", 1200000,411027)
+print(harry.name, harry.role,harry.salary,harry.pincode)
