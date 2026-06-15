943f21a4245276666a25533251d06725883890cc
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-15 21:41:51 +0530

    recent update

---
+++ chapter10_OOP/practice_question/2.problem.py
@@ -0,0 +1,10 @@
+# 2. Write a class “Calculator” capable of finding square, cube and square root of a number
+class calculator:
+    def __init__(self, n):
+       self.number = n
+        
+    def add(self):
+        print(f"addition is {self.n+self.n}")
+    
+calc = calculator(4)
+calc.add()
