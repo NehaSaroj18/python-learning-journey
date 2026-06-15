a055ee585e6c1ffaa48ea15816d27441198122d6
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2026-06-15 20:56:54 +0530

    recent update

---
+++ chapter10_OOP/04_constructor.py
@@ -0,0 +1,23 @@
+class Employee:
+    language = "Python";
+    salary = 120000;
+    
+    def __init__(self):
+        print(f"Hello , Here is your information {self.name}")
+    
+    @staticmethod
+    def greet():
+        print("Good morning")
+        
+    def info(self):
+        print(f"The language is {self.language} and the salary is (self.salary)")
+        
+
+harry = Employee()
+harry.name = "Harry"
+print(harry.name , harry.salary)
+
+Radhe = Employee()
+Radhe.name = "Radhe"
+print(Radhe.name , Radhe.language, Radhe.salary )
+
